51bc66d4b65f74a323444ad2de125e7cb1d97276
agusmel <agusmel03@live.com.ar>
2025-09-09 00:50:40 -0300

    [feature] show free seats

--- models/asientosModel.js
+++ models/asientosModel.js
@@ -11,7 +11,19 @@ const obtenerTodosLosAsientos = (idVuelo, callback) => {
 };
 
 
+// Obtener asientos libres
+const obtenerAsientosLibres = (idVuelo, callback) => {
+    const query = "SELECT * FROM asiento WHERE Id_vuelo = ? AND Estado = 'disponible'";
+    db.query(query, [idVuelo], (err, results) => {
+        if (err) return callback(err);
+        callback(null, results);
+    });
+};
+
+
+
 module.exports = {
     obtenerTodosLosAsientos,
-  
+    obtenerAsientosLibres
+
 };
