e9fa07721866c7409650bab0b750694704242be7
agusmel <agusmel03@live.com.ar>
2025-09-09 15:54:28 -0300

    [feature] cancel seats

--- routes/reservasRoutes.js
+++ routes/reservasRoutes.js
@@ -5,4 +5,7 @@ const reservasController = require('../controllers/reservasController.js');
 // Ruta para reservar un asiento disponible de un vuelo específico
 router.post('/asientos/:idVuelo/reservar', reservasController.reservarAsiento);
 
+// Ruta para cancelar una reserva (pasar de confirmado a disponible)
+router.post('/asientos/:idVuelo/cancelar', reservasController.cancelarAsiento);
+
 module.exports = router;
