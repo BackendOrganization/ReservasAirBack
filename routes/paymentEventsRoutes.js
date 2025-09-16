0a7cfe47720201b301b06824209a94739aad56c6
JuanManuelSosa2004 <juanmanuelsvccnt@gmail.com>
2025-09-15 21:25:40 -0300

    WIP: failed payments

--- routes/paymentEventsRoutes.js
+++ routes/paymentEventsRoutes.js
@@ -6,4 +6,7 @@ router.post('/payment/confirm', paymentEventsController.confirmPayment);
 
 router.post('/payment/cancel', paymentEventsController.cancelPayment);
 
+// Nuevo endpoint para marcar pago como fallido y crear evento
+router.post('/payment/fail', paymentEventsController.failPayment);
+
 module.exports = router;
