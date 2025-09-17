387dc5e71cc7c36023220bf0a51fc9be5f3211bf
agusmel <agusmel03@live.com.ar>
2025-09-17 14:50:31 -0300

    [feature/flights information]

--- routes/flightsRoutes.js
+++ routes/flightsRoutes.js
@@ -3,8 +3,13 @@ const express = require('express');
 const router = express.Router();
 const flightsController = require('../controllers/flightsController.js');
 
+// Ruta para insertar un vuelo
 router.post('/flights/ingest', flightsController.ingestFlight);
 
+// Ruta para obtener todos los vuelos
+router.get('/flights', flightsController.getAllFlights);
+
+// Ruta para cancelar reservas de un vuelo
 router.post('/flights/:externalFlightId/cancel', flightsController.cancelFlightReservations);
 
 module.exports = router;
