1dd5f94471193b5b246f393977501a7a99e823be
agusmel <agusmel03@live.com.ar>
2025-10-14 20:42:47 -0300

    fix-ingest flights and EndPoint flightStatus DELAYED

--- routes/flightsCartsRoutes.js
+++ routes/flightsCartsRoutes.js
@@ -8,6 +8,7 @@ router.post('/flight-cart/add', flightCartsController.addFlightToCart);
 
 router.post('/flight-cart/remove', flightCartsController.removeFlightFromCart);
 
+//cambiar despues a metodo get sacando el id del usuario del token
 router.post('/flight-cart/get', flightCartsController.getCartByUserId);
 
 module.exports = router;
