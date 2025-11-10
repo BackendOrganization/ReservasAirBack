02848b093a9ce8e89b980311c092c62962d621c5
JuanManuelSosa2004 <juanmanuelsvccnt@gmail.com>
2025-11-10 11:56:14 -0300

    Delete logic for users

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { createUser, getUser } = require('../controllers/usersController');
+const { createUser, getUser, deleteUser } = require('../controllers/usersController');
 
 router.post('/', createUser);
 
 router.get('/:id', getUser);
+
+router.delete('/:id', deleteUser);
 
 module.exports = router;
